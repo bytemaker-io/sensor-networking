e05509d37aca905fbd209ce8baf165092b7e9da9
Michal Jason <michaljason@MichaldeMacBook-Pro.local>
2023-03-28 20:59:18 +0200

    second commit

--- first-lab.go
+++ first-lab.go
@@ -1,44 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
-
-	"github.com/tidwall/gjson"
-)
+import "fmt"
 
 func main() {
-	//get the weather data from openweathermap.org
-	city := "Budapest"
-	apiKey := "3d58c5af590dc4dfaaf561febf931a3b"
-	//create the query url
-	queryUrl := "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey
-	//make the request
-	resp, err := http.Get(queryUrl)
-	//check if the request failed
-	if err != nil {
-		fmt.Println("Request failed:", err)
-		return
-	}
-	//close the body
-	defer resp.Body.Close()
-	//read the body
-	body, err := ioutil.ReadAll(resp.Body)
-	//check if the read failed
-	if err != nil {
-		fmt.Println("Read body failed:", err)
-		return
-	}
-	//convert the body to string
-	jsonStr := string(body)
-	//parse the json string and get the temperature from jsonStr
-	kelvinTemperature := gjson.Get(jsonStr, "main.temp")
-	//convert the temperature from kelvin to celsius
-	celsiusTemperature := kelvinTemperature.Float() - 273.15
-	//convert the temperature from kelvin to fahrenheit
-	fahrenheitTemperature := celsiusTemperature*9/5 + 32
-	fmt.Println("Temperature in Budapest:", celsiusTemperature, "Celsius")
-	fmt.Println("Temperature in Budapest:", kelvinTemperature, "kelvin")
-	fmt.Println("Temperature in Budapest:", fahrenheitTemperature, "Fahrenheit")
+	fmt.Println("嗨客网(www.haicoder.net)")
+	defer func() {
+		if info := recover(); info != nil {
+			fmt.Println("触发了宕机, Info =", info)
+		} else {
+			fmt.Println("程序正常退出")
+		}
+	}()
+	fmt.Println("Hai")
+	fmt.Println("Coder")
+	panic("fatal error")
+	fmt.Println("Over")
+
 }
---
+++ second-lab.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/tarm/serial"
+)
+
+/*
+*
+Author: Wang Fei (Kaelan)
+institute: VIK
+Date: 2023-3-28
+Description: This program is used to read the data from the serial port and storage the data to a csv file.
+*
+*/
+func getdata() {
+	// open the serial port,the baud rate is 9600
+	config := &serial.Config{Name: "/dev/virtual-device", Baud: 9600}
+	// open the serial port
+	port, err := serial.OpenPort(config)
+	//return the error if the serial port is not open
+	if err != nil {
+		panic(err)
+	}
+	//when finish the program,close the serial port
+	defer port.Close()
+
+	// open the output file,storage the data on a csv file
+	f, err := os.Create("temperature-data-sensor.csv")
+	//return the error if the file is not open
+	if err != nil {
+		panic(err)
+	}
+	//when finish the program,close the file
+	defer f.Close()
+	//create a csv writer(stream)
+	writer := csv.NewWriter(f)
+	//when finish the program,flush the data to the file
+	defer writer.Flush()
+
+	// write the header,the first column of the csv file is time,the second column is the temperature data
+	writer.Write([]string{"time", "data"})
+
+	// read the data from the serial port
+	for {
+		buf := make([]byte, 128)
+		n, err := port.Read(buf)
+		if err != nil {
+			fmt.Println("failed read data from the sensor device", err)
+			break
+		}
+
+		// convert the data to string
+		data := string(buf[:n])
+		data = data[:len(data)-2] // remove the "\r of each line
+		// get the current time
+		t := time.Now().Format(time.RFC3339)
+		// if the data is "END",break the loop
+		if data == "END" {
+			break
+		}
+		// if the data is a number,write the data to the csv file
+		if _, err := strconv.Atoi(data); err == nil {
+			writer.Write([]string{t, data})
+		}
+	}
+}
